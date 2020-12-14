ad82b953e6c424910f1a1e4791ded86616943655
mateuszfranke <mateuszfranke@icloud.com>
2020-12-14 14:25:18 +0100

    working on success component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoadComponent } from './load/load.component';
+import { SuccessfulComponent } from './successful/successful.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoadComponent
+    LoadComponent,
+    SuccessfulComponent
   ],
   imports: [
     BrowserModule,
